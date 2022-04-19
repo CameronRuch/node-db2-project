cfe97c7ad8678400f873df6e4ab22402751ba7ef
CameronRuch <cameron.ruch@yahoo.com>
2022-04-19 17:30:45 -0500

    all test passing

--- api/server.js
+++ api/server.js
@@ -1,7 +1,24 @@
 const express = require("express")
 
+const carRouters = require('./cars/cars-router');
+
 const server = express()
 
 // DO YOUR MAGIC
+
+server.use(express.json())
+
+server.use('/api/cars', carRouters);
+
+server.use('*', (req, res, next) => {
+    next({ status: 404, message: 'not found!'})
+})
+
+server.use((err, req, res, next) => {
+    res.status(err.status || 500).json({
+        message: err.message
+    })
+})
+
 
 module.exports = server
